b695e6db7e459ec96513a7185e5fe91df435a666
Jackcubbi <jackcubbi@gmail.com>
2024-08-16 23:35:55 +0300

    edit: product card styles

--- items.php
+++ items.php
@@ -8,18 +8,24 @@ $items = $stmt->fetchAll(PDO::FETCH_ASSOC); ?>
 
 <section class="products">
   <div class="container">
-    <h2>Список товаров</h2>
-    <ul>
-      <?php foreach ($items as $item): ?>
-        <li>
-          <img src="uploads/<?= $item['image']; ?>" alt="<?= $item['name']; ?>" width="100">
-          <h3><?= $item['name']; ?></h3>
-          <p><?= $item['description']; ?></p>
-          <p>Продавец: <?= $item['username']; ?></p>
-          <a href="trade.php?item_id=<?= $item['id']; ?>">Предложить обмен</a>
-        </li>
-      <?php endforeach; ?>
-    </ul>
+    <div class="items-container">
+      <h2>Список товаров</h2>
+      <ul>
+        <?php foreach ($items as $item): ?>
+          <li>
+            <?php if (empty($item['image'])) : ?>
+              <img src="uploads/no-image.gif" alt="no-image">
+            <?php else : ?>
+              <img src="uploads/<?= $item['image']; ?>" alt="<?= $item['name']; ?>" width="100">
+            <?php endif; ?>
+            <h3><?= $item['name']; ?></h3>
+            <p><?= $item['description']; ?></p>
+            <p>Продавец: <?= $item['username']; ?></p>
+            <a href="trade.php?item_id=<?= $item['id']; ?>">Предложить обмен</a>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    </div>
   </div>
 </section>
 
