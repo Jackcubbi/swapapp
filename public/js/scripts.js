5c575887dcfde598422cc99b89918592de7128d8
Jackcubbi <jackcubbi@gmail.com>
2024-08-22 22:49:21 +0300

    edit: account h2 style

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,13 +10,14 @@ window.addEventListener("scroll", () => {
   }
 });
 
-
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // Функция для подтверждения предложений обмена
   const tradeLinks = document.querySelectorAll('a[href^="trade.php"]');
-  tradeLinks.forEach(link => {
-    link.addEventListener('click', function (event) {
-      const confirmation = confirm('Вы уверены, что хотите предложить обмен этим товаром?');
+  tradeLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      const confirmation = confirm(
+        "Вы уверены, что хотите предложить обмен этим товаром?"
+      );
       if (!confirmation) {
         event.preventDefault();
       }
@@ -24,30 +25,30 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // Всплывающее уведомление при успешных действиях
-  const notification = document.querySelector('.notification');
+  const notification = document.querySelector(".notification");
   if (notification) {
     setTimeout(() => {
-      notification.style.display = 'none';
+      notification.style.display = "none";
     }, 3000);
   }
 });
 
-
 let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-const dots = document.querySelectorAll('.nav-dot');
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".nav-dot");
 
 function showSlide(index) {
   const totalSlides = slides.length;
   currentSlide = (index + totalSlides) % totalSlides;
-  document.querySelector('.slider').style.transform = 'translateX(-${currentSlide * 33.33}%)';
+  document.querySelector(".slider").style.transform =
+    "translateX(-${currentSlide * 33.33}%)";
 
-  dots.forEach(dot => dot.classList.remove('active'));
-  dots[currentSlide].classList.add('active');
+  dots.forEach((dot) => dot.classList.remove("active"));
+  dots[currentSlide].classList.add("active");
 }
 
 dots.forEach((dot, index) => {
-  dot.addEventListener('click', () => showSlide(index));
+  dot.addEventListener("click", () => showSlide(index));
 });
 
 // Автоматическая смена слайдов каждые 5 секунд
@@ -55,4 +56,4 @@ setInterval(() => {
   showSlide(currentSlide + 1);
 }, 5000);
 
-showSlide(0); 
+showSlide(0);
