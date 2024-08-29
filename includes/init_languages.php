8593efe65a7d56e2079574f527adc68717736cb4
Jackcubbi <jackcubbi@gmail.com>
2024-08-29 20:54:11 +0300

    add: change_language.php

--- includes/init_languages.php
+++ includes/init_languages.php
@@ -7,10 +7,11 @@ use Symfony\Component\Translation\Translator;
 use Symfony\Component\Translation\Loader\ArrayLoader;
 
 // Initializing the translator
-$lang = new Translator('ru'); // Язык по умолчанию
+$lang = new Translator('ru'); // Default language
 $lang->addLoader('array', new ArrayLoader());
-$lang->addResource('array', include 'includes/languages/en.php', 'en');
 $lang->addResource('array', include 'includes/languages/ru.php', 'ru');
+$lang->addResource('array', include 'includes/languages/en.php', 'en');
+
 
 // Add the translator to global variables if needed
 $GLOBALS['lang'] = $lang;
