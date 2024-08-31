bb1557f2d2f97574c70b401fef14a22cc9d38e2a
Jackcubbi <jackcubbi@gmail.com>
2024-08-31 17:56:43 +0300

    edit: footer flex grid

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,30 @@
 <footer>
   <div class="container">
     <div class="footer-columns">
-      <ul class="footer-menu">
+      <ul class="footer-menu column">
         <li><a href=" index.php"><?= __('main_page'); ?></a></li>
         <li><a href="items.php"><?= __('products'); ?></a></li>
+      </ul>
+
+      <div class="footer-user-menu column">
+
+
+      </div>
+
+      <div class="footer-privacy-menu column">
+
+
+      </div>
+
+      <div class="socials column">
+        <a href="#">Facebook</a>
+        <a href="#">Twitter</a>
+        <a href="#">Instagram</a>
+      </div>
+
+      <div class="rights column">&copy; <?= '2020 - ' . date("Y") . ' ' . __('all_rights_reserved'); ?></div>
+
+      <div class="footer-lang column">
         <form action="change_language.php" method="POST">
           <select name="lang" onchange="this.form.submit()">
             <option value="ru" <?= $GLOBALS['lang']->getLocale() === 'ru' ? 'selected' : ''; ?>>Русский</option>
@@ -11,22 +32,7 @@
             <!-- Добавьте другие языки по мере необходимости -->
           </select>
         </form>
-      </ul>
-      <div class="footer-user-menu">
-
-
       </div>
-      <div class="footer-privacy-menu">
-
-
-      </div>
-      <div class="socials">
-        <a href="#">Facebook</a>
-        <a href="#">Twitter</a>
-        <a href="#">Instagram</a>
-      </div>
-
-      <div class="">&copy; <?= '2020 - ' . date("Y") . ' ' . __('all_rights_reserved'); ?></div>
     </div>
   </div>
 </footer>
